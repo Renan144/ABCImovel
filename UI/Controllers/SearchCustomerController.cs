aef3effcd363ff37cdf2d75c28458c96aa6e053a
Renan144 <110985578+Renan144@users.noreply.github.com>
2022-09-01 21:08:45 -0300

    Add files via upload

--- UI/Controllers/SearchCustomerController.cs
+++ UI/Controllers/SearchCustomerController.cs
@@ -5,11 +5,11 @@ namespace UI.Controllers {
 	public class SearchCustomerController:Controller {
 		public IActionResult Index (string? select, string? selTxt) {
 			if (select!=null&&selTxt!=null) {
-				var sql = "SELECT * FROM clientes WHERE "+select+" LIKE '"+selTxt+"%'";
-				var customer = new Consult().ResultsDb(sql);
-				ViewBag.Customers=customer;
+				var sql = "SELECT * FROM clientes WHERE "+select+" LIKE '%"+selTxt+"%'";
+				var customers = new Consult().ResultsDb(sql);
+				ViewBag.Customers=customers;
+				ViewBag.SelTxt=selTxt;
 				ViewBag.Success=true;
-				Console.WriteLine(sql);
 			}
 			return View();
 		}
